209c2eaec798283246201bf843cbff62368d3efd
Dima12411 <fixer40delta@gmail.com>
2021-11-11 11:35:11 +0300

    first

---
+++ my-app/src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://www.ukinbound.org/wp-content/uploads/2019/04/Seaside-towns-875x350.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
